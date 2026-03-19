36bf3eaaf4e7ce3d2cabdc8e95ebd988ccebdc6b
Lucas S Gums <123860836+Evigno@users.noreply.github.com>
2026-03-19 15:30:53 -0300

    2º commit (ListaEncadeada e Aluno)

---
+++ colecao/Aluno.java
@@ -0,0 +1,15 @@
+package colecao;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private double nota;
+
+    public Aluno(String nome, String matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    
+}
