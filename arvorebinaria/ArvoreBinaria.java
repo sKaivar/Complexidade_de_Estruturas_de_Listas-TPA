6370a35b0543f7eff7319e99e4e4a81ad06b472a
julianorhcp83 <julianomarins62@gmail.com>
2026-05-11 14:55:47 -0300

    3rd: conclusao arvore binaria

--- arvorebinaria/ArvoreBinaria.java
+++ arvorebinaria/ArvoreBinaria.java
@@ -1,14 +1,11 @@
 package arvorebinaria;
 
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
-
-import colecao.IColecao;
 import lstencadeada.No;
 
 public class ArvoreBinaria<T> extends ArvoreBinariaBase<T>  {
     protected No<T> raiz;
+    private int tamNo;
     
     public ArvoreBinaria(Comparator<T> comparador){
         super(comparador);
@@ -25,10 +22,10 @@ public class ArvoreBinaria<T> extends ArvoreBinariaBase<T>  {
             return -1;
         }
 
-        int alturaAnt = calcularAltura(no.getEsquerda());
-        int alturaProx = calcularAltura(no.getDireita());
+        int alturaEsq = calcularAltura(no.getEsquerda());
+        int alturaDir = calcularAltura(no.getDireita());
 
-        return Math.max(alturaAnt, alturaProx) + 1;
+        return Math.max(alturaEsq, alturaDir) + 1;
     }
 
     @Override
@@ -40,36 +37,68 @@ public class ArvoreBinaria<T> extends ArvoreBinariaBase<T>  {
         StringBuilder sb = new StringBuilder();
         sb.append("[\n");
 
-        Queue<No<T>> fila = new LinkedList<>();
-        fila.add(this.raiz);
+        int h = altura();
+        for(int i = 0; i <= h; i++){
+            StringBuilder linha = new StringBuilder();
+            gerarLinha(this.raiz, i, linha);
 
-        while (!fila.isEmpty()){
-            int tam = fila.size();
-
-            for(int i = 0; i < tam ; i++){
-                No<T> atual = fila.poll();
+            if(linha.length() > 2){
+                linha.setLength(linha.length() - 2);
             }
+            sb.append(linha.toString()).append("\n");
+            
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private void gerarLinha(No<T> no, int nivel, StringBuilder sb){
+        if(no == null) return;
+        if(nivel == 0){
+            sb.append(no.getValor().toString()).append(", ");
+        }else{
+            gerarLinha(no.getEsquerda(), nivel - 1, sb);
+            gerarLinha(no.getDireita(), nivel - 1, sb);
         }
     }
     @Override
     public String caminharEmOrdem(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        caminharEmOrdemRecursivo(this.raiz, sb);
+        if (sb.length() > 1){
+            sb.setLength(sb.length() -2);
+        }
+
+        sb.append("]");
+        return sb.toString();
 
     }
 
+    private void caminharEmOrdemRecursivo(No<T> no, StringBuilder sb){
+        if (no == null) return;
+        caminharEmOrdemRecursivo(no.getEsquerda(), sb);
+        sb.append(no.getValor().toString()).append(", ");
+        caminharEmOrdemRecursivo(no.getDireita(), sb);
+        
+    }
+    // metodos de IColecao
     @Override
     public void adicionar(T novoValor){
-        this.raiz = adicionarRecursivo(No<T> atual, T valor);
+        this.raiz = adicionarRecursivo(this.raiz, novoValor);
         
     }
     private No<T> adicionarRecursivo(No<T> atual, T valor){
         if(atual == null){
+            this.tamNo++;
             return new No<T>(valor);
         }
         int comp = this.comparador.compare(valor, atual.getValor());
         if(comp < 0){
-            atual.setAnterior(adicionarRecursivo(atual.getAnterior(), valor));
+            atual.setEsquerda(adicionarRecursivo(atual.getEsquerda(), valor));
         } else if(comp > 0){
-            atual.setProximo(adicionarRecursivo(atual.getProximo(), valor));
+            atual.setDireita(adicionarRecursivo(atual.getDireita(), valor));
         }
         return atual;
     }
@@ -87,12 +116,14 @@ public class ArvoreBinaria<T> extends ArvoreBinariaBase<T>  {
         int comp = this.comparador.compare(valor, atual.getValor());
 
         if (comp == 0){
-            return atual.getValor();
-        }else if(comp < 0){
+            return atual.getValor();        
+        }
+        if(comp < 0){
             return pesquisarRecursivo(atual.getEsquerda(), valor);
         }else{
             return pesquisarRecursivo(atual.getDireita(), valor);
         }
+
     }
     
     @Override
@@ -102,6 +133,7 @@ public class ArvoreBinaria<T> extends ArvoreBinariaBase<T>  {
         }
 
         this.raiz = removerRecursivo(this.raiz, valor);
+        this.tamNo--;
         return true;
     }
 
@@ -114,18 +146,25 @@ public class ArvoreBinaria<T> extends ArvoreBinariaBase<T>  {
         if (comp < 0){
             atual.setEsquerda(removerRecursivo(atual.getEsquerda(), valor));
         }else if (comp > 0){
-            atual.setDireita(removerRecursivo(atual.getDireita(), valor))
+            atual.setDireita(removerRecursivo(atual.getDireita(), valor));
         }else{
-            if (atual.getEsquerda == null){
-                return atual.getDireita();
-            }else if (atual.getDireita == null){
-                return atual.getEsquerda();
+            if(atual.getEsquerda() == null) return atual.getDireita();
+            if(atual.getDireita() == null) return atual.getEsquerda();
+
+            No<T> sucessor = atual.getDireita();
+            while(sucessor.getEsquerda() != null){
+                sucessor = sucessor.getEsquerda();
             }
+
+            T menor = sucessor.getValor();
+            atual.setValor(menor);
+            atual.setDireita(removerRecursivo(atual.getDireita(), menor));
         }
+        return atual;
     }
 
     @Override
     public int quantidadeNos(){
-
+        return this.tamNo;
     }
 }    
